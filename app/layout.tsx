40a4156357ce9d8344598006273fe0b4376739f0
Jahid0178 <zahidul.haque767@gmail.com>
2026-01-19 17:49:03 +0600

    initial file upload

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title:
+    "Edvanta – Modern LMS & Online Education Platform Template built with Next.js",
+  description:
+    "Edvanta is a modern LMS & online education template built with Next.js, perfect for courses, e-learning platforms, and educational startups.",
+  keywords: [
+    "lms",
+    "learning management system",
+    "education",
+    "online course",
+    "nextjs",
+    "react",
+    "dashboard",
+    "e-learning",
+    "admin panel",
+    "school",
+    "university",
+    "training",
+    "saas",
+    "edtech",
+  ],
+  authors: [
+    {
+      name: "StackStudio",
+      url: "https://themeforest.net/user/stackstudiotech",
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
