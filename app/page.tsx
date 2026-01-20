45dcea83dc13aaef08f39ea568ce09252bf96fb5
Jahid0178 <zahidul.haque767@gmail.com>
2026-01-21 00:49:22 +0600

    feat: add client logo marquee section
    - implement auto-scrolling client logo section
    - optimize client logos using next/image
    - centralize client assets in data file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "@/components/layout/Header/Header";
 import HeroSection from "@/components/containers/home/HeroSection";
+import ClientSection from "@/components/containers/home/ClientSection";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header />
       <HeroSection />
+      <ClientSection />
     </React.Fragment>
   );
 };
--- data/index.ts
+++ data/index.ts
@@ -22,3 +22,12 @@ export const headerNavigationLinks: HeaderNavigationLink[] = [
     href: "/contact-us",
   },
 ];
+
+export const clients: string[] = [
+  "/images/clients/client-1.png",
+  "/images/clients/client-2.png",
+  "/images/clients/client-3.png",
+  "/images/clients/client-4.png",
+  "/images/clients/client-5.png",
+  "/images/clients/client-6.png",
+];
