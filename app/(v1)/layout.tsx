9775a77c388c19b3a62155cfa4e6bcbca26a7bfc
Jahid0178 <zahidul.haque767@gmail.com>
2026-01-30 21:03:54 +0600

    refactor: removing old footer folder

--- app/(v1)/layout.tsx
+++ app/(v1)/layout.tsx
@@ -1,12 +1,12 @@
 import Header from "@/components/layout/Header/Header";
-import Footer from "@/components/layout/Footer/Footer";
+// import Footer from "@/components/layout/Footer/Footer";
 
 const VersionOneLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
